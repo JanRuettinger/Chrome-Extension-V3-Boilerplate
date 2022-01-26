ba5408e16b71213e279bac04700eae7eae571608
Jan Ruettinger <service+github@janruettinger.com>
2022-01-26 17:40:26 -0600

    inital commit

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: 'always',
+  singleQuote: true,
+  tabWidth: 2,
+  semi: false,
+  // https://zhuanlan.zhihu.com/p/64627216
+  htmlWhitespaceSensitivity: 'ignore',
+  trailingComma: 'all',
+  tailwindConfig: './tailwind.config.js',
+}
---
+++ src/content/index.tsx
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom'
+// eslint-disable-next-line node/no-unpublished-import
+import 'tailwindcss/tailwind.css'
+
+import Content from './Content'
+
+const container = document.createElement('div')
+document.documentElement.prepend(container)
+
+ReactDOM.render(<Content />, container)
